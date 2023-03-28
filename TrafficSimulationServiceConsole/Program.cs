f5d59d4ef568ff94ee3cfc6490b3c0607197a780
Mikkel Bach Nielsen <minielsen@microsoft.com>
2023-03-28 10:56:18 +0200

    test

--- TrafficSimulationServiceConsole/Program.cs
+++ TrafficSimulationServiceConsole/Program.cs
@@ -21,7 +21,8 @@ ILogger<Program> logger = provider.GetRequiredService<ILogger<Program>>();
 
 int lanes = 3;
 CameraSimulation[] cameras = new CameraSimulation[lanes];
-
+Console.WriteLine(string.IsNullOrEmpty(Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING"))? "No Conn string": Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING"));
+logger.LogInformation(string.IsNullOrEmpty(Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING"))? "No Conn string": Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING"));
 for (var i = 0; i < lanes; i++)
 {
     var camNumber = i + 1;
